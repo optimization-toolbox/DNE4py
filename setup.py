9cd5b25e8a66792f48857677d60fbce94de3dab4
Hugodovs <hugodovs@gmail.com>
2020-06-10 23:13:34 -0400

    setup python >= 3.5

--- setup.py
+++ setup.py
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: GNU General Public License v3.0",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.5',
 )
