a4f49ad9e4b7373734e41b8554b47e6aa284b3e3
Hugodovs <hugodovs@gmail.com>
2020-06-24 23:00:56 -0400

    example working!

--- neuroevolution4py/examples/main.py
+++ neuroevolution4py/examples/main.py
@@ -8,14 +8,7 @@ from neuroevolution4py.optimizers.deepga.mutation import Member
 
 
 def objective(x):
-
     result = np.sum(x**2)
-
-    x1 = x[0].round(10)
-    x2 = x[1].round(10)
-    y = result.round(10)
-    # print(f'function: x = [{x1},{x2}], f = {y}')
-
     return result
 
 
@@ -24,9 +17,9 @@ def train(steps):
 
     optimizer = TruncatedRealMutatorGA(objective=objective,
                                        initial_guess=initial_guess,
-                                       workers_per_rank=128,
+                                       workers_per_rank=10,
                                        num_elite=1,
-                                       num_parents=10,
+                                       num_parents=3,
                                        sigma=sigma,
                                        verbose=True,
                                        seed=2,
@@ -40,23 +33,23 @@ def test(steps):
     global initial_guess, sigma
 
     # Load weights:
-    #join_mpidata("costs", nb_workers=2, nb_generations=steps)
-    #join_mpidata("genotypes", nb_workers=2, nb_generations=steps)
+    join_mpidata("costs", nb_workers=2, nb_generations=steps)
+    join_mpidata("genotypes", nb_workers=2, nb_generations=steps)
     costs = load_mpidata("costs")
     genotypes = load_mpidata("genotypes")
 
     # Training performance
-    #means = np.mean(costs, axis=1)
-    #stds = np.std(costs, axis=1)
-    #mins = np.min(costs, axis=1)
-    #maxes = np.max(costs, axis=1)
+    means = np.mean(costs, axis=1)
+    stds = np.std(costs, axis=1)
+    mins = np.min(costs, axis=1)
+    maxes = np.max(costs, axis=1)
 
-    #plt.errorbar(np.arange(steps), means, stds, fmt='ok', lw=3)
-    # plt.errorbar(np.arange(steps), means, [means - mins, maxes - means],
-    #             fmt='.k', ecolor='gray', lw=1)
-    #plt.xlim(-1, steps)
-    #plt.xticks(np.arange(-1, steps + 1, 1.0))
-    # plt.show()
+    plt.errorbar(np.arange(steps), means, stds, fmt='ok', lw=3)
+    plt.errorbar(np.arange(steps), means, [means - mins, maxes - means],
+                 fmt='.k', ecolor='gray', lw=1)
+    plt.xlim(-1, steps)
+    plt.xticks(np.arange(-1, steps + 1, 1.0))
+    plt.show()
 
     # Check same training:
     #new_costs = []
@@ -71,42 +64,8 @@ def test(steps):
     #    curr_costs = np.array(curr_costs)
     #    new_costs.append(curr_costs)
     #new_costs = np.array(new_costs)
-
     #np.save(open("new_costs", "wb"), new_costs)
-    new_costs = np.load("new_costs")
-    a = np.sort(costs)
-    b = np.sort(new_costs)
-    print(a[1])
-    print(b[1])
-    print(np.equal(a[1], b[1]))
-    exit()
-
-    print(np.equal(new_costs[1], costs[1]))
-    print(new_costs[1])
-    print("\n\n")
-    print(costs[1])
-
-    # print(costs)
-    # print(genotypes)
-
-#    for g in range(len(genotypes)):
-
-#        # print(f"Generation: {g}")
-#        population_genotype = genotypes[g]
-#        print(population_genotype)
-#        print("\n\n")
-#        continue
-
-#        for genotype in population_genotype:
-#            #print(initial_guess, sigma, genotype)
-#            member = Member(initial_guess, genotype, sigma)
-#            phenotype = member.phenotype
-
-#            x1 = phenotype[0].round(10)
-#            x2 = phenotype[1].round(10)
-#            y = objective(phenotype).round(10)
-#            print(f'load: [{x1},{x2}], {y}')
-#        print("\n")
+    #new_costs = np.load("new_costs")
 
 
 if "__main__" == __name__:
@@ -114,5 +73,5 @@ if "__main__" == __name__:
     initial_guess = np.array([1.0, 1.0])
     sigma = 0.1
 
-    # train(100)
-    test(100)
+    # train(20)
+    test(20)
